940156eed5239fe32ad014335628fdbd890485c8
Prathap <prathap.ufttest@gmail.com>
2019-12-21 19:23:45 +0530

    secong commit

--- src/test/java/CGI/TM_HRM/LogInHRM.java
+++ src/test/java/CGI/TM_HRM/LogInHRM.java
@@ -4,19 +4,28 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LogInHRM {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:\\PavanSelenium\\Driver\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://testingmasters.com/hrm/symfony/web/index.php/auth/login");
+		driver.get("http://testingmasters.com/hrm/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
 		driver.findElement(By.id("txtUsername")).sendKeys("user02");
 		driver.findElement(By.id("txtPassword")).sendKeys("TM1234");
 		driver.findElement(By.id("btnLogin")).click();
+		WebDriverWait wait=new WebDriverWait(driver,5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='welcome']"))).click();
+		
+		driver.findElement(By.xpath("//a[text()='Logout']")).click();
+		driver.close();
+		
 
 	}
 
